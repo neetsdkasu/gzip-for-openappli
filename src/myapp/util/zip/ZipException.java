461e329d8f1f8c497f0ee89a3ae31b91c79d6df3
Leonardone <neetsdkasu@yahoo.co.uk>
2013-12-30 05:13:24 +0900

    Create ZipException.java
    Zipフォーマットエラー用らしいので追加

---
+++ src/myapp/util/zip/ZipException.java
@@ -0,0 +1,15 @@
+package myapp.util.zip;
+
+import java.io.IOException;
+
+public class ZipException extends IOException {
+
+	public ZipException() {
+		super();
+	}
+	
+	public ZipException(String s) {
+		super(s);
+	}
+	
+}
